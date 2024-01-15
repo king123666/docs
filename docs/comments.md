cee0dbc37f4204ff4fb7a9e28875b3f4439140d1
mikigo <huangmingqiang@uniontech.com>
2024-01-15 09:37:38 +0800

    update

--- docs/comments.md
+++ docs/comments.md
@@ -1,3 +1,4 @@
 ---
+lastUpdated: false
 comment: true
 ---
--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 set -e
-pnpm add -g pnpm
+#pnpm add -g pnpm
 pnpm add -D vitepress
 pnpm i vite-plugin-vitepress-auto-sidebar
 pnpm i vitepress-plugin-comment-with-giscus
