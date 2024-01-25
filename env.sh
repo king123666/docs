0c3a041614822e65cd2b5a73ac8a53b248e76f16
mikigo <huangmingqiang@uniontech.com>
2024-01-25 11:40:48 +0800

    fix:
    Description:
    
    Log:

--- env.sh
+++ env.sh
@@ -7,5 +7,5 @@ pnpm i vitepress-plugin-back-to-top
 pnpm add -D busuanzi.pure.js
 pnpm add -D markdown-it-mathjax3
 pnpm i @mermaid-js/mermaid-mindmap@9.3.0 mermaid@9.1.0 vitepress-plugin-mermaid@2.0.10
-pnpm medium-zoom
+pnpm i medium-zoom
 pnpm add -D vitepress-markdown-timeline
